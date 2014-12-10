580457f7975a99e691521be9a87dcd99c1efcff5
Brandonmason1997 <brandonmason1997@gmail.com>
2014-12-10 16:22:07 +1100

    First Attempt at RTC

--- GPS_Tracker_Main_Sketch/GPS_Tracker_Main_Sketch.ino
+++ GPS_Tracker_Main_Sketch/GPS_Tracker_Main_Sketch.ino
@@ -1,9 +1,36 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Wire.h>
+#include "RTClib.h"
+  //Library for Real Time Clock, setting the file name
+#include <TinyGPS++.h>
+  //The cornerstone of the project
+#include <SoftwareSerial.h>
+  //Communicating with GPS I suppose
+#include <SPI.h>
+  //Something to do with Data Logging Sheild
+#include <SD.h>
+  //The SD Library
 
+const int chipSelect = 10;
+  //This is for the Datalogger to communicate with the Uno
+static const int RXPin = 4, TXPin = 3;
+  //Pins assigned for GPS
+static const uint32_t GPSBaud = 9600;
+  //GPS Baudrate
+TinyGPSPlus gps;
+SoftwareSerial ss(RXPin, TXPin);
+  // The serial connection to the GPS device
+RTC_DS1307 rtc;
+String fileName = "POOP";
+
+void setup() {
+  Wire.begin();
+  rtc.begin();
+  Serial.begin(9600);
+  DateTime now = rtc.now();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  Serial.print(now.year(), DEC);
+  Serial.print(fileName);
+  delay(3000);
 }
